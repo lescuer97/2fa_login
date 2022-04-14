f1a44ff6a12b2ddf6faef9a14c56b09d147fb3f0
leonardo <leoescuer@protonmail.com>
2022-04-14 19:28:30 +0200

    svelte first commit

--- front-login/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-interface ImportMetaEnv {
-  readonly VITE_DEVELOP_SERVER: string;
-  // more env variables...
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
